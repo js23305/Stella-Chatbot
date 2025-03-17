761d1341a1ef79a38c49e1b92726d148860509ba
js23305 <jasminesingla652@gmail.com>
2025-03-17 19:10:41 +0530

    Chatbot first version

--- stella_chatbot/movie_chatbot/urls.py
+++ stella_chatbot/movie_chatbot/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('movie_Chatbot/', views.movie_chatbot, name='movie_chatbot'),
+    path('movie_chatbot/', views.movie_chatbot, name='movie_chatbot'),
 
 ]
--- stella_chatbot/movie_chatbot/views.py
+++ stella_chatbot/movie_chatbot/views.py
@@ -12,4 +12,34 @@ def movie_chatbot(request):
         'content': 'Welcome to the chatbot'
     }
 
-    return HttpResponse(template.render())
+    return HttpResponse(template.render(context))
+
+def send_film_to_user(data):
+
+    print(data.POST)
+
+    message = "I'm not sure"
+
+    match data.POST['genre']:
+       case "Horror":
+            message = "I recommend you watch The Conjuring"
+
+       case "Romance":
+            message = "I recommend you watch The Notebook"
+
+       case "Comedy":
+            message = "I recommend you watch Superbad"
+
+       case "War":
+            message = "I recommend you watch Saving Private Ryan"
+
+       case "Western":
+            message = "I recommend you watch The Good, the Bad and the Ugly"
+       case _:
+            print("The language doesn't matter, what matters is solving problems.")
+
+    return JsonResponse({
+         "message": message,
+         "status_Code": 200
+    })
+
